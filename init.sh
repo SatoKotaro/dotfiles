0b870b8aa8559bc5bd62130ec8d1d215ed9b7632
Kotaro Sato <punkrou404@google.com>
2019-04-14 11:20:09 +0000

    どの場所からでもinitializeできるようになった

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_DIRECTORY="$(cd $(dirname $0); pwd)"
 DOT_TARBALL="https://github.com/punkrou404/dotfiles.git"
 
-
-cd ${DOT_DIRECTORY}
+echo ${DOT_DIRECTORY}
 
 for f in .??*
 do
@@ -12,10 +11,7 @@ do
   [[ ${f} = ".gitignore" ]] && continue
   [[ ${f} = "README.md" ]] && continue
   [[ ${f} = "`basename $0`" ]] && continue
-    
+
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
-
-
-
