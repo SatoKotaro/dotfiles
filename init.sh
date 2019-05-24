ea944d853a84cbe9f84c45fcfe16f416a115fc25
Kotaro Sato <punkrou404@google.com>
2019-05-24 21:52:21 +0900

    初期化ファイルにコメントを入力

--- init.sh
+++ init.sh
@@ -7,11 +7,15 @@ echo ${DOT_DIRECTORY}
 
 for f in .??*
 do
+  # デプロイしないファイルを弾く
   [[ ${f} = ".git" ]] && continue
   [[ ${f} = ".gitignore" ]] && continue
   [[ ${f} = "README.md" ]] && continue
   [[ ${f} = "`basename $0`" ]] && continue
 
+  # ユーザルート配下にシンボリックリンクを貼る
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
+
+# 成功時のメッセージ
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
