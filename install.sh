e7b6589a32531c4980a15e93e9d97334be6f0f2e
Kotaro Sato <punkrou404@gmail.com>
2019-11-25 07:57:33 +0900

    init時のpyenv非存在エラーをfix

--- install.sh
+++ install.sh
@@ -16,3 +16,14 @@ git clone https://github.com/punkrou404/upowerw.git
 cd upowerw
 sh ./init.sh
 
+# pyenv
+rm -rf ~/.pyenv
+mkdir ~/.pyenv
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+# pyenv-virtualenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# virtualenv
+sudo pip install --upgrade pip
+sudo pip install virtualenv
